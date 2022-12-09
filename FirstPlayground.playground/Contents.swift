17a6e21d5a29889005dfe80a54826664c4a54128
Amanda Detofol <amandadetofol@MacBook-Pro-de-Amanda.local>
2022-12-09 08:18:59 -0300

    [Swift Fundamentals]: first palyground

---
+++ FirstPlayground.playground/Contents.swift
@@ -0,0 +1,8 @@
+//usar camelCase para declarar as variaveis
+var playerName = "Alice"
+var age = 21
+var temperature = 72.6
+var activeMemberShip = true
+
+//neste caso o swift esta inferindo o tipo das variaveis
+age = age + 5
