1da6a72d3217f864179c715e04656a9afb0a64f6
Amanda Detofol <amandadetofol@MacBook-Pro-de-Amanda.local>
2022-12-09 08:38:58 -0300

    [Swift Fundamentals]: variables

---
+++ Variables.playground/Contents.swift
@@ -0,0 +1,10 @@
+
+var name: String = "Ana"
+var editsPermited: Bool = true
+var mazximumPixel: Int  = 3480
+
+var bonusScore: Int //nenhum espaco deve ser inserido entre o nome da variavel e o :
+var environmentName: String
+var levelCompleted: Bool
+var progressPercentage: Double
+
